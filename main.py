f4094b01139e262f01e41fe6119e48bf8f7d51dc
XSET-Clix <shahded2124@gmail.com>
2024-11-20 20:29:54 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,61 +1,21 @@
 import random
-cc = ["rock", "paper", "scissors"]
-rc = random.choice(cc)
-#rock
-def rock(uc):
-    if rc == "rock":
-        if uc == "rock":
-            print("Tie")
-        elif uc == "paper":
-            print("You win!")
-        elif uc == "scissors":
-            print("You lose")
-        else:
-            print("Invalid Option")
-#paper
-def paper(uc):
-    if rc == "paper":
-        if uc == "paper":
-            print("Tie")
-        elif uc == "scissors":
-            print("You win!")
-        elif uc == "rock":
-            print("You lose")
-        else:
-            print("Invalid Option")
-#scissors            
-def scissors(uc):
-    if rc == "scissors":
-        if uc == "scissors":
-            print("Tie")
-        elif uc == "rock":
-            print("You win!")
-        elif uc == "paper":
-            print("You lose")
-        else:
-            print("Invalid Option")
-#while loop
-def game():
-    choice = input("Enter Your Choice:")
-    while True:
-        if choice.lower() == "rock":
-            rock(choice)
-        elif choice.lower() == "paper":
-            paper(choice)
-        elif choice.lower() == "scissor":
-            scissors(choice)
-        else:
-            print("Invaild Option")
-        print("Do you wanna continue?")
-        option = input("yes/no")
-        if option.lower() == "yes":
-            game()
-        else:
-            print("Thank you playing")
-            break
-        
-
-
-if __name__ == "__main__":
-    print("Welcome to the Rock, Paper, and, Scissor Game.")
-    game()
+#intialize the function
+def diceroll():
+    print("Welcome to Diceroll.")
+    startroll = (input("Type roll to roll the dice:"))
+    score = 0
+    dice = random.randint(1,6)
+    if startroll == "roll":
+        print(dice)
+    coinflip = (input("Type flip to flip the coin"))
+    values = ["heads", "tails"]
+    if coinflip == "flip":
+        coin = random.choice(values)
+        print(coin)
+    if coin == "heads":
+        score = score + dice
+    elif coin == "tails":
+        score = score - dice
+    print("Your score is:", score)
+#call function
+diceroll()
